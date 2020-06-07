05aad57c3e3c775ee91b2c034336977f666c893b
pierreemad93 <pierreemad93@outlook.com>
2020-06-07 14:16:10 +0200

    init app

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/front', function () {
     return view('front.index');
 });
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
