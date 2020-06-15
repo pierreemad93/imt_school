734a8dcf1fed362c3e649430c3f7bc65fb483d95
pierreemad93 <pierreemad93@outlook.com>
2020-06-15 15:55:55 +0200

    init app

--- resources/views/front/includes/about_us.blade.php
+++ resources/views/front/includes/about_us.blade.php
@@ -12,17 +12,22 @@
                         <div class="owl-stage" style="transform: translate3d(-1650px, 0px, 0px); transition: all 0s ease 0s; width: 3300px;">
                             <div class="owl-item cloned" style="width: 540px; margin-right: 10px;">
                                 <div>
-                                    <img alt="" class="img-fluid"
-                                         src="{{asset('assets/front/img/generic/generic-corporate-3-3-full.jpg')}}">
+                                    <img alt="" class="img-fluid" src="{{asset('assets/front/img/generic/about-1.jpg')}}">
+                                </div>
+                            </div>
+                            <div class="owl-item" style="width: 540px; margin-right: 10px;">
+                                <div>
+                                    <img alt="" class="img-fluid" src="{{asset('assets/front/img/generic/about-2.jpg')}}">
                                 </div>
                             </div>
                         </div>
                     </div>
+
                     <div class="owl-nav disabled">
                         <button type="button" role="presentation" class="owl-prev"></button>
                         <button type="button" role="presentation" class="owl-next"></button>
                     </div>
-                    <div class="owl-dots">
+                    <div class="owl-dots disabled">
                         <button role="button" class="owl-dot active"><span></span></button>
                         <button role="button" class="owl-dot "><span></span></button>
                     </div>
@@ -32,12 +37,10 @@
                 <div class="overflow-hidden mb-2">
                     <h2 class="text-color-dark font-weight-normal text-5 mb-0 pt-0 mt-0 appear-animation animated maskUp appear-animation-visible"
                         data-appear-animation="maskUp" data-appear-animation-delay="1200"
-                        style="animation-delay: 1200ms;">Who <strong class="font-weight-extra-bold">We Are</strong>
+                        style="animation-delay: 1200ms;">About <strong class="font-weight-extra-bold">US</strong>
                     </h2>
                 </div>
-                <p class="appear-animation animated fadeInUpShorter appear-animation-visible"
-                   data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1400"
-                   style="animation-delay: 1400ms;">IMT School (I Make Technology School) is not a place where you can
+                <p class="appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1400" style="animation-delay: 1400ms;">IMT School (I Make Technology School) is not a place where you can
                     take some courses; it is a place in which you practice technology. We believe that listening to
                     someone speaking about something is not a good way of learning, so, “Do it yourself” is our way.
 
