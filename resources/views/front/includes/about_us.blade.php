752921c1a05979e4372d42e1e8c8b55b3e035b82
pierreemad93 <pierreemad93@outlook.com>
2020-06-15 18:02:18 +0200

    init app

--- resources/views/front/includes/about_us.blade.php
+++ resources/views/front/includes/about_us.blade.php
@@ -1,56 +1,36 @@
-<section class="section section-default border-0 my-5 appear-animation animated fadeIn appear-animation-visible"
-         data-appear-animation="fadeIn" data-appear-animation-delay="750" style="animation-delay: 750ms;">
-    <div class="container py-4">
-        <div class="row align-items-center">
-            <div class="col-md-6 appear-animation animated fadeInLeftShorter appear-animation-visible"
-                 data-appear-animation="fadeInLeftShorter" data-appear-animation-delay="800"
-                 style="animation-delay: 800ms;">
-                <div class="owl-carousel owl-theme nav-inside mb-0 owl-loaded owl-drag owl-carousel-init"
-                     data-plugin-options="{'items': 1, 'margin': 10, 'animateOut': 'fadeOut', 'autoplay': true, 'autoplayTimeout': 3000, 'loop': true}"
-                     style="height: auto;">
-                    <div class="owl-stage-outer">
-                        <div class="owl-stage" style="transform: translate3d(-1650px, 0px, 0px); transition: all 0s ease 0s; width: 3300px;">
-                            <div class="owl-item cloned" style="width: 540px; margin-right: 10px;">
-                                <div>
-                                    <img alt="" class="img-fluid" src="{{asset('assets/front/img/generic/about-1.jpg')}}">
-                                </div>
-                            </div>
-                            <div class="owl-item" style="width: 540px; margin-right: 10px;">
-                                <div>
-                                    <img alt="" class="img-fluid" src="{{asset('assets/front/img/generic/about-2.jpg')}}">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="owl-nav disabled">
-                        <button type="button" role="presentation" class="owl-prev"></button>
-                        <button type="button" role="presentation" class="owl-next"></button>
-                    </div>
-                    <div class="owl-dots disabled">
-                        <button role="button" class="owl-dot active"><span></span></button>
-                        <button role="button" class="owl-dot "><span></span></button>
-                    </div>
-                </div>
+<section class="pt-5 appear-animation" data-appear-animation="fadeInUpShorter">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 p-0">
+                <section class="parallax section section-parallax match-height border-top-0" data-plugin-parallax=""
+                         data-plugin-options="{'speed': 1.5}"
+                         data-image-src="{{asset('assets/front/img/generic/about-2.jpg')}}"
+                         style="min-height: 260px; height: 315px; position: relative; overflow: hidden;">
+                    <div class="parallax-background"
+                         style="background-image: url(&quot;img/parallax/parallax-2.jpg&quot;); background-size: cover; position: absolute; top: 0px; left: 0px; width: 100%; height: 180%; transform: translate3d(0px, -91.1429px, 0px); background-position-x: 50%;"></div>
+                </section>
             </div>
-            <div class="col-md-6">
-                <div class="overflow-hidden mb-2">
-                    <h2 class="text-color-dark font-weight-normal text-5 mb-0 pt-0 mt-0 appear-animation animated maskUp appear-animation-visible"
-                        data-appear-animation="maskUp" data-appear-animation-delay="1200"
-                        style="animation-delay: 1200ms;">About <strong class="font-weight-extra-bold">US</strong>
-                    </h2>
-                </div>
-                <p class="appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1400" style="animation-delay: 1400ms;">IMT School (I Make Technology School) is not a place where you can
-                    take some courses; it is a place in which you practice technology. We believe that listening to
-                    someone speaking about something is not a good way of learning, so, “Do it yourself” is our way.
+            <div class="col-lg-6 p-0">
+                <section class="section section-primary pl-4 pr-4 match-height border-top-0" style="height: 315px;">
+                    <div class="pr-5">
+                        <span class="d-block font-weight-light negative-ls-1 text-5 mb-1"><em class="text-2 opacity-8">About US</em></span>
+                        <h2 class="font-weight-extra-bold text-color-dark negative-ls-3 line-height-1 text-7 mb-3"><em>SOME
+                                WORDS ABOUT US</em></h2>
+                        <p class="lead mb-4 pb-2">
+                            IMT School (I Make Technology School) is not a place where you can take some courses; it is
+                            a place in which you practice technology. We believe that listening to someone speaking
+                            about something is not a good way of learning, so, “Do it yourself” is our way.
 
-                    Our learning methodology totally depends on hands on labs that transfers the knowledge you get from
-                    being just information to be an experience.</p>
-                <p class="mb-0 appear-animation animated fadeInUpShorter appear-animation-visible font-weight-extra-bold"
-                   data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1600"
-                   style="animation-delay: 1600ms;"> if you want to go Professional, IMT School is your destination!
-                    Let’s meet the experts, let’s practice technology.</p>
+                            Our learning methodology totally depends on hands on labs that transfers the knowledge you
+                            get from being just information to be an experience.
+
+                            Our staff are engineers from leading companies in the same field.
+                        </p>
+                    </div>
+                </section>
             </div>
         </div>
-
     </div>
+
 </section>
+
