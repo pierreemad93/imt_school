335922045d1a510649df164e59b59aaeb363619f
pierreemad93 <pierreemad93@outlook.com>
2020-07-20 14:24:08 +0200

    init app

--- resources/views/front/courses/arm.blade.php
+++ resources/views/front/courses/arm.blade.php
@@ -47,44 +47,96 @@
                     <div class="col-lg-8">
                         <div class="tab-pane tab-pane-navigation active" id="tabsNavigation1">
                             <h4>Details</h4>
-                            <p>The diploma is supervised by Eng. Ahmed Ashmawy (Embedded Systems department manager in
-                                ITI).</p>
-                            <p>80% of the diploma is about making your own embedded system (Set your HW environment and
-                                start developing your projects) and the rest 20% is embedded systems concepts that you
-                                must know.</p>
+                            <p>IMT is offering professional ARM Programming Full Diploma with total duration 120 hours.
+                                By the end of the diploma, students shall deliver a professional graduation project utilizing all
+                                the concepts learned during the course on the presence of high-level committee from the
+                                industry.
+                                </p>
+                            <p>Our instructors are well qualified to teach and have a long experience in teaching Embedded
+                                Systems. All of them with no exception are teaching in ITI. We use a unified content selected
+                                carefully and ensure all technical and practical aspects.
+                                Most of our diploma is about making your own embedded system (Set your HW environment
+                                and start developing your projects). Each student gets a very professional kit developed by our
+                                team and its components are listed below</p>
                         </div>
                         <div class="tab-pane tab-pane-navigation" id="tabsNavigation2">
                             <h4>Price</h4>
-                            <p>3000 L.E. (Development KIT Included).</p>
+                            <p>4000 L.E. (Development KIT Included).</p>
                         </div>
                         <div class="tab-pane tab-pane-navigation" id="tabsNavigation3">
                             <h4>Duration</h4>
-                            <p>Total of 160 hours.</p>
+                            <p>Total of 120 hours.</p>
                             <p>2 days/week with total of 8 hours: 4 hours each day --> Friday & Saturday.</p>
                         </div>
                         <div class="tab-pane tab-pane-navigation" id="tabsNavigation4">
                             <h4>Development KIT</h4>
-                            <p>AVR Atmega32 Microcontroller and AVR Programmer.</p>
-                            <p>Motors (DC, Servo and stepper motor).</p>
-                            <p>Character LCD and Seven Segment Displays.</p>
-                            <p>Sensors (Temperature and Light).</p>
-                            <p>Basic elements (Relay, Buzzer, Tactile switches, LEDs, Transistors, Darlington pair,
-                                Optocouplers, Op Amp, Etc).</p>
-                            <p>EEPROM</p>
-                            <p>Other elements (USB to TTL converter, Keypad ...).</p>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <ul>
+                                        <li>STM32 Blue Pill board based on STM32F103C8T6 Microcontroller</li>
+                                        <li>Color TFT Display ST7335</li>
+                                        <li>WiFi Module 8266</li>
+                                        <li>8 Bit R2R DAC with LM386 Audio Amplifier and AUX Socket</li>
+                                        <li>8x8 788BS LED Matrix Dispay</li>
+                                        <li>Digital Universal Standard IR Receiver</li>
+                                        <li>Digital RGB LED</li>
+                                        <li>Serial To Parallel 74HC595</li>
+                                    </ul>
+                                </div>
+                                <div class="col-lg-6">
+                                    <ul>
+                                        <li>Buzzer, LEDs and Tactile Switches</li>
+                                        <li>Breadboard 170 Points</li>
+                                        <li>STD ARM Debugger supports SW and JTAG with 10 Pin Flat Cable</li>
+                                        <li>NEC Compatible Remote Control</li>
+                                        <li>Stereo AUX standard Speaker</li>
+                                        <li>PL2303 USB to TTL Module</li>
+                                        <li>
+                                                40 F-F Jumper wire
+                                            <br>
+                                                10 M-F Jumper wire
+                                            <br>
+                                                10 M-M Jumper wire
+                                        </li>
+                                        <li>Plastic Box for keeping all components</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                         <div class="tab-pane tab-pane-navigation" id="tabsNavigation5">
                             <h4>Course Outline</h4>
-                            <p>C Programming and Data Structures Algorithms.</p>
-                            <p>Embedded Systems Concepts.</p>
-                            <p>Embedded C for Micro-controllers.</p>
-                            <p>Embedded Systems Software Design Using UML.</p>
-                            <p>HW/SW Co-Design.</p>
-                            <p>Real Time Operating Systems and Scheduling.</p>
-                            <p>Software Engineering for Embedded Systems.</p>
-                            <p>Verification and Testing of Embedded Systems.</p>
-                            <p>Embedded System Development Tools.</p>
-                            <p>Graduation project.</p>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <p>ARM Architecture</p>
+                                    <p>RCC Peripheral</p>
+                                    <p>DIO Peripheral</p>
+                                    <p>NVIC</p>
+                                    <p>NVIC Driver</p>
+                                    <p>EXTI</p>
+                                    <p>SysTick</p>
+                                    <p>OS Scheduler</p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p>LED Matrix</p>
+                                    <p>DAC</p>
+                                    <p>Audible Signals</p>
+                                    <p>Serial To Parallel</p>
+                                    <p>IR Communication</p>
+                                    <p>UART</p>
+                                    <p>SPI and TFT</p>
+                                    <p>DMA</p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p>WiFi Module</p>
+                                    <p>Web Development</p>
+                                    <p>IOT</p>
+                                    <p>Story of Flashing</p>
+                                    <p>Hex Parser</p>
+                                    <p>Bootloader Implementation</p>
+                                    <p>Workshop</p>
+                                    <p>Project Defense </p>
+                                </div>
+                            </div>
                         </div>
                         @if(Auth::check())
                             <form method="post" action="{{route('courses.store')}}">
@@ -92,12 +144,12 @@
                                 <input type="hidden" value="{{Auth::user()->name}}" name="name">
                                 <input type="hidden" value="{{Auth::user()->phone}}" name="phone">
                                 <input type="hidden" value="{{Auth::user()->email}}" name="email">
-                                <input type="hidden" value="full-diploma-embeddsystems" name="coursename">
+                                <input type="hidden" value="arm-fulldiploma" name="coursename">
                                 <input type="hidden" value="0" name="pending">
                                <input type="submit" value="Enroll" class="btn btn-primary btn-lg">
                             </form>
                             @else
-                         <a href="{{route('login')}}" class="btn btn-info btn-lg">sing in to Enroll</a>
+                         <a href="{{route('login')}}" class="btn btn-info btn-lg">Login in to Enroll</a>
                         @endif
                     </div>
                 </div>
